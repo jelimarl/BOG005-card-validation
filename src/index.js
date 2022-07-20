65fb1507aee0ff738cbef16ad1da0f66a3fb08e1
jelimarl <jelimarlm@gmail.com>
2022-07-19 20:14:46 -0500

    Funciones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import validator from './validator.js';
+let cardNumber = document.getElementById('cardNumber');
+const payButton = document.getElementById('payButton');
+let showIsValid = document.getElementById('showIsValid');
 
+function showValidation () {
+    let creditCardNumber = cardNumber.value;
+
+    if (creditCardNumber=='') {
+        alert('Escribe tu número de tarjeta de crédito');
+    }
+    else {
+        let lastFourDigits = validator.maskify(creditCardNumber);
+        cardNumber.value = lastFourDigits;
+        
+        let validation = validator.isValid(creditCardNumber);
+
+        if (validation==true) {
+            let valid = "Aprobado";
+            showIsValid.innerHTML = valid;
+        }
+        else {
+            let invalid = "Tarjeta inválida";
+            showIsValid.innerHTML = invalid;
+        }
+    } 
+}
+
+payButton.addEventListener("click", showValidation);
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,53 @@
 const validator = {
   // ...
+  isValid: function(creditCardNumber) {
+    let arrayReversed = creditCardNumber.split('').reverse();
+    
+    for (let i = 0; i < arrayReversed.length; i++) {
+      if (i%2!=0) {
+        arrayReversed[i] = arrayReversed[i]*2;
+        if (arrayReversed[i] > 9) {
+          let getDigits = arrayReversed[i].toString().split('');
+          let addDigits = 0;
+          getDigits.forEach(function(element) {
+            addDigits += parseInt(element);
+          });
+          arrayReversed[i] = addDigits;
+        }
+        else {
+          arrayReversed[i] = arrayReversed[i]*1;
+        }
+      }
+      else {
+        arrayReversed[i] = arrayReversed[i]*1;
+      }
+    }
+
+    let addTotal = 0;
+    arrayReversed.forEach(function(value) {
+      addTotal += value;
+    });
+    let residue = addTotal%10;
+
+    if (residue==0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  },
+
+  maskify: function(creditCardNumber) {
+    if (creditCardNumber.length > 4) {
+      let hideNumbers = "#".repeat(creditCardNumber.length - 4);
+      let showLastFour = creditCardNumber.substring(creditCardNumber.length - 4);
+      let mask = hideNumbers + showLastFour;
+      return mask;
+    }
+    else {
+      return creditCardNumber;
+    } 
+  }
 };
 
 export default validator;
